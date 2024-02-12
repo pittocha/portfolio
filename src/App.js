0eed4f092a9753d7e1f3d280e09e53e854f7290c
Nicolas Martinez <kurulimpah@gmail.com>
2024-02-12 14:30:19 +0100

    ajout de section about me et skills

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import ContactForm from './components/ContactForm/ContactForm';
+import AboutMe from './components/AboutMe/AboutMe';
+import Competences from './components/skills/Competences';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
+      <Competences />
       <header className="App-header">
         😶‍🌫️
       </header>
