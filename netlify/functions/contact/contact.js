0e4c16e51e533e6873db24330d5a438675e8aa6a
Nicolas Martinez <kurulimpah@gmail.com>
2024-03-06 12:09:33 +0100

    correction d'une erreur de typo

--- netlify/functions/contact/contact.js
+++ netlify/functions/contact/contact.js
@@ -1,16 +1,42 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const nodemailer = require('nodemailer');
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    // Parse the request body
+    const requestBody = JSON.parse(event.body);
+
+    // extract form data
+    const name = requestBody.name;
+    const email = requestBody.email;
+    const message = requestBody.message;
+
+    // configuration de nodemailer
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'kurulimpah@gmail.com',
+          pass: process.env.GMAIL_PASS,
+      },
+    });
+
+    // Preparer mail option
+    const mailOption = {
+      from: name,
+      to: 'kurulimpah@gmail.com',
+      subject: 'Nouveau message du formulaire du portfolio',
+      text: `Nom: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    // Envoi du mail
+    await transport.sendMail(mailOption);
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+      body: JSON.stringify({ message: 'Email envoyé'})
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    return { statusCode: 500, body: error.toString() };
   }
 }
 
