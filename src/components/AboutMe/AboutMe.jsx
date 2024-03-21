944610c2280727516b635dcc5b2875aadf8ef7c6
Nicolas Martinez <kurulimpah@gmail.com>
2024-03-21 15:19:35 +0100

    application des modifications demandées

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,6 +8,7 @@ function AboutMe () {
         <div className="presentation-text">
         <h1>Nicolas Martinez</h1>
         <p>Développeur/intégrateur web.
+           Aprées une formation de développeur web chez openclassroom.
            Je travaille en HTML, CSS, JavaScript, REACT et nodejs,
            mon objectif est de donner forme à votre projet jusqu'à son déploiement,
            je suis bassiste amateur et j'ai un intérêt grandissant pour la communauté open-source, cherchant des solutions efficaces pour la Musique Assistée par Ordinateur (MAO).</p>
