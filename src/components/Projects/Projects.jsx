3cb2738d3868a91b714adb4d8d05d1b1c3cd578a
Nicolas Martinez <kurulimpah@gmail.com>
2024-10-16 12:31:19 +0200

    correction de l'ortographe

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -105,14 +105,14 @@ function Projects () {
         {
             title: 'portfolio de photographe en javascript',
             image: BluelCard,
-            description: "Recupérer des données utilisateur via des formulaires, Manipuler les éléments du DOM, gérer les événements utilisateur avec javascript. Le plus gros chalenge que j'ai rencontré sur ce projet etait le codage du modal from scratch en javascript natif.",
+            description: "Récupérer des données utilisateur via des formulaires, Manipuler les éléments du DOM, gérer les événements utilisateur avec javascript. Le plus gros challenge que j'ai rencontré sur ce projet était le codage du modal from scratch en javascript natif.",
             githubRepo: 'https://github.com/pittocha/sophiebluel',
             gallery: bluelGallery
         },
         {
-            title: "front-end d'un site de reservation en React",
+            title: "front-end d'un site de réservation en React",
             image: KasaCard,
-            description:"Configurer ls navigation avec React Router, Développer les éléments du site avec des composants React. Sur ce projet la plus grosse difficultée etait l'apprentissage de SASS et tout particliérement des animations et de l'utilisation de fonctions. ",
+            description:"Configurer la navigation avec React Router, Développer les éléments du site avec des composants React. Sur ce projet la plus grosse difficulté était l'apprentissage de SASS et tout particuliérement des animations et de l'utilisation de fonctions. ",
             githubRepo: 'https://github.com/pittocha/Kasa',
             gallery: kasaGallery
 
@@ -120,7 +120,7 @@ function Projects () {
         {
             title: "Backend d'un site de notation de livres en Node.js",
             image: GrimmoireCard,
-            description: "Mettre en œuvre les opération crud de manière sécurisé, stocker des données de manière sécurisé, Implémenter un système d'authentification sécurisé, gérer l'optimisation de fichier avec multer et sharp.Dans le projet Back End la plus grosse dificultée etait l'integration d'outils de green code ici multer qui est un outils relativement puissant et qui à nécessité un certain temps d'apprentissage pour comprendre l'étendue des possibilité qu'il offre.",
+            description: "Mettre en œuvre les opération crud de manière sécurisé, stocker des données de manière sécurisé, Implémenter un système d'authentification sécurisé, gérer l'optimisation de fichier avec multer et sharp.Dans le projet Back End la plus grosse dificulté était l'intégration d'outils de green code ici multer qui est un outils relativement puissant et qui à nécessité un certain temps d'apprentissage pour comprendre l'étendue des possibilité qu'il offre.",
             githubRepo: 'https://github.com/pittocha/grimoireback',
             gallery: grimmoireGallery
         }
